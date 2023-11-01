0d7431f0b7fd72b6e0db9d9329e342fb30dfe2e3
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-11-01 17:40:43 +0900

    ✨ [Feat] : HealthCheck API 작성

---
+++ src/main/java/net/spofo/auth/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package net.spofo.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+}
